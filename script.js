a646649c3f8a464f52e300b741a1e2c2b5558b28
akeshma <akeshma@gmail.com>
2021-07-14 09:36:53 +0300

    Overview

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello!')
+// function changeFn() {
+//   // var el = document.getElementById('start');
+//   // console.log(el);
+//   // el.innerHTML = 'Go';
+//   $('#start').html('Go');
+// }
+// $(document).ready(function() {
+//   $('#start').html('Gocx');
+// });
+$(function() {
+  $('#start').html('Go');
+});
+function changeFn(){
+
+}
